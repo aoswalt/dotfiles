faca91c604f7ef03254c2a8fe767231eb88620f6
Adam Oswalt <email@adamoswalt.com>
2016-10-26 20:20:41 -0500

    rough setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,23 @@
-#! /bin/sh
+#! /bin/bash
 
-echo "Placeholder file"
+# get script's path
+pushd $(dirname $0) > /dev/null
+this_dir=$(pwd -P)
+popd > /dev/null
+
+home_dir=$(echo $HOME)
+
+# simple files
+files=(".bashrc" ".vimrc.after" ".vimrc.before" ".zshrc")
+num_files=${#files[@]}
+
+for (( index=0; index<${num_files}; ++index )); do
+  ln -s $this_dir/${files[$index]} $home_dir/${files[$index]}
+done
+
+# special case files
+terminator_path=$home_dir/.config/terminator
+mkdir -p "${terminator_path}"
+ln -s "${this_dir}/terminator-config" "${terminator_path}/config"
+
+ln -s "${this_dir}/aliases.zsh" "${home_dir}/.oh-my-zsh/custom/aliases.zsh"
