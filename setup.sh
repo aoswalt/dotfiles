3405b4dab119cccabb82a40519b6bd0d2cb39871
Adam Oswalt <email@adamoswalt.com>
2016-11-24 21:42:36 -0600

    drop terminator for tmux

--- setup.sh
+++ setup.sh
@@ -8,17 +8,12 @@ popd > /dev/null
 home_dir=$(echo $HOME)
 
 # simple files
-files=(".bashrc" ".vimrc.after" ".vimrc.before" ".zshrc")
+files=(".bashrc" ".vimrc.after" ".vimrc.before" ".zshrc" ".tmux.conf")
 num_files=${#files[@]}
 
 for (( index=0; index<${num_files}; ++index )); do
   ln -s $this_dir/${files[$index]} $home_dir/${files[$index]}
 done
-
-# special case files
-terminator_dir=$home_dir/.config/terminator
-mkdir -p "${terminator_dir}"
-ln -s "${this_dir}/terminator-config" "${terminator_dir}/config"
 
 ln -s "${this_dir}/aliases.zsh" "${home_dir}/.oh-my-zsh/custom/aliases.zsh"
 
