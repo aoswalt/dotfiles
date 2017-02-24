e6cd959be5d2448ce311c1b6d660a135d7f57691
Adam Oswalt <email@adamoswalt.com>
2017-02-23 18:58:17 -0600

    trimming some fat

--- README.md
+++ README.md
@@ -4,11 +4,17 @@
 
 My miscellaneous config files for setting up my shell.
 
-**Currently expects Oh My Zsh and the Janus vim config to be setup.**
-
 Files are setup by creating symlinks pointing to files in the repo to allow for
 easy updating.
 
 ## Setup
 
 Run `setup.sh` to create the symlinks.
+
+## Soft Dependencies
+
+* tmux
+* oh-my-zsh
+* neovim
+* ag - the silver searcher
+* Konsole (on Ubuntu for ligatures)
--- setup.sh
+++ setup.sh
@@ -8,22 +8,14 @@ popd > /dev/null
 home_dir=$(echo $HOME)
 
 # simple files
-files=(".bashrc" ".vimrc.after" ".vimrc.before" ".zshrc" ".tmux.conf" ".eslintrc.json" ".Xresources")
+files=(".bashrc" ".zshrc" ".tmux.conf" ".eslintrc.json" ".Xresources")
 num_files=${#files[@]}
 
 for (( index=0; index<${num_files}; ++index )); do
   ln -s $this_dir/${files[$index]} $home_dir/${files[$index]}
 done
 
-# special case files
-# need check because follows symlink otherwise
-tmux_dir=$home_dir/.tmux
-if [ ! -L "${tmux_dir}" ]; then
-  ln -s "${this_dir}/.tmux" "${tmux_dir}"
-else
-  echo "Tmux folder already exits"
-fi
-
+# special case for nested file
 ln -s "${this_dir}/aliases.zsh" "${home_dir}/.oh-my-zsh/custom/aliases.zsh"
 
 # need check because follows symlink otherwise
@@ -33,13 +25,6 @@ if [ ! -L "${snippets_dir}/snippets" ]; then
   ln -s "${this_dir}/snippets" "${snippets_dir}/snippets"
 else
   echo "Snippets folder exists in janus config"
-fi
-
-# clone needed repos
-if [ ! -d "${home_dir}/.janus/onedark" ]; then
-  git clone git@github.com:aoswalt/onedark.vim.git $home_dir/.janus/onedark
-else
-  echo "onedark folder exists"
 fi
 
 nvim_dir=$home_dir/.config/nvim
