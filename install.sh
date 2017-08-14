c27076eb69416b7b2e9aaafdd434cafadad5d331
Adam Oswalt <email@adamoswalt.com>
2017-08-14 15:09:26 -0500

    -n test does not work as expected but raw test on variable works

--- install.sh
+++ install.sh
@@ -12,22 +12,28 @@ function try_link() {
     echo -e "\033[36m$dst_path\033[0m already exists. \033[31;1mRemove?\033[0m [y/\033[31;1mN\033[0m]"
     read choice
     if [[ $choice =~ ^[Yy] ]]; then
-      [ -n $verbose ] && echo "Removing $dst_path"
+      [ $verbose ] && echo "Removing \033[36m$dst_path\033[0m"
       rm $dst_path
+    else
+      [ $verbose ] && echo "Not removing \033[36m$dst_path\033[0m"
     fi
   fi
 
   if [ ! -e $dst_path ] && ln -s $src_path $dst_path; then
-    [ -n $verbose ] && echo "Link created for $dst_path"
+    [ $verbose ] && echo "Link created for \033[36m$dst_path\033[0m"
+  else
+    [ $verbose ] && echo "Could not create link for \033[36m$dst_path\033[0m"
   fi
 }
 
+# print help and exit
 function print_help_abort() {
   echo 'USAGE: Options'
   exit 0
 }
 
 
+# display help if no arguments
 if (($# == 0)); then
   print_help_abort
 fi
@@ -48,7 +54,6 @@ while getopts 'Af:khptvz' flag; do
     *) error "Unexpected option ${flag}" ;;
   esac
 done
-
 
 
 # simple files in home folder
@@ -73,7 +78,7 @@ nvim_dir=$HOME/.config/nvim
 mkdir -p ${nvim_dir}
 try_link $this_dir/init.vim $nvim_dir/init.vim
 
-if [ -x "$(command -v konsole)" ] && [ -n $konsole_files ]; then
+if [ -x "$(command -v konsole)" ] && [ $konsole_files ]; then
   konsole_profile_dir=$HOME/.local/share/konsole
   mkdir -p $konsole_profile_dir
   try_link $this_dir/konsole/Mine.profile $konsole_profile_dir/Mine.profile
@@ -81,8 +86,11 @@ if [ -x "$(command -v konsole)" ] && [ -n $konsole_files ]; then
 fi
 
 # prezto setup
-if [ -n $clone_prezto ]; then
-  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+if [ $setup_prezto ]; then
+  if [[ ! -e "${ZDOTDIR:-$HOME}/.zprezto"  ]]; then
+    [ $verbose ] && echo "Cloning Prezto"
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  fi
 fi
 
 # TODO(adam): set to zsh
