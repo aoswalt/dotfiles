c5902af25619d7c88e896df09cf0b2c256324042
Adam Oswalt <email@adamoswalt.com>
2017-08-14 08:31:56 -0500

    rough outline of option flag setup

--- install.sh
+++ install.sh
@@ -12,14 +12,44 @@ function try_link() {
     echo -e "\033[36m$dst_path\033[0m already exists. \033[31;1mRemove?\033[0m [y/\033[31;1mN\033[0m]"
     read choice
     if [[ $choice =~ ^[Yy] ]]; then
+      [ -n $verbose ] && echo "Removing $dst_path"
       rm $dst_path
     fi
   fi
 
-  if ln -s $src_path $dst_path; then
-    echo "Link created for $dst_path"
+  if [ ! -e $dst_path ] && ln -s $src_path $dst_path; then
+    [ -n $verbose ] && echo "Link created for $dst_path"
   fi
 }
+
+function print_help_abort() {
+  echo 'USAGE: Options'
+  exit 0
+}
+
+
+if (($# == 0)); then
+  print_help_abort
+fi
+
+
+while getopts 'Af:khptvz' flag; do
+  case "${flag}" in
+    A) all='true' ;;
+    h) print_help_abort ;;
+    k) konsole_files='true' ;;
+    p) clone_prezto='true' ;;
+    t) setup_tmux='true' ;;
+    v) verbose='true' ;;
+    z) set_zsh='true' ;;
+    \?) print_help_abort ;;
+
+    f) files="${OPTARG}" ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+
 
 # simple files in home folder
 files=(
@@ -43,26 +73,15 @@ nvim_dir=$HOME/.config/nvim
 mkdir -p ${nvim_dir}
 try_link $this_dir/init.vim $nvim_dir/init.vim
 
-if [ -x "$(command -v konsole)" ]; then
+if [ -x "$(command -v konsole)" ] && [ -n $konsole_files ]; then
   konsole_profile_dir=$HOME/.local/share/konsole
   mkdir -p $konsole_profile_dir
   try_link $this_dir/konsole/Mine.profile $konsole_profile_dir/Mine.profile
   try_link $this_dir/konsole/Mine.colorscheme $konsole_profile_dir/Mine.colorscheme
 fi
 
-# while getopts 'abf:v' flag; do
-#   case "${flag}" in
-#     a) aflag='true' ;;
-#     b) bflag='true' ;;
-#     f) files="${OPTARG}" ;;
-#     v) verbose='true' ;;
-#     *) error "Unexpected option ${flag}" ;;
-#   esac
-# done
-
-getopts 'p' clone_prezto
 # prezto setup
-if [ -e $clone_prezto ]; then
+if [ -n $clone_prezto ]; then
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
 
