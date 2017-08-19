1ebb6116020efdae41684b46ab70385c9f1c5f65
Adam Oswalt <email@adamoswalt.com>
2017-08-18 19:35:27 -0500

    use [[ ]] instead of simple [ ] test for robustness

--- install.sh
+++ install.sh
@@ -9,26 +9,26 @@ function try_link() {
   dst_path=$2
 
   if [[ -e $dst_path || -L $dst_path ]]; then
-    if [ $all_yes ]; then
+    if [[ $all_yes ]]; then
       choice='y'
-    elif [ ! -z $all_no ]; then
+    elif [[ ! -z $all_no ]]; then
       choice='n'
     else
-      read -p $'\n\033[36m'"$dst_path"$'\033[0m already exists. \033[31;1mRemove?\033[0m [y/\033[31;1mN\033[0m]:  ' choice
+      read -p $'\033[36m'"$dst_path"$'\033[0m already exists. \033[31;1mRemove?\033[0m [y/\033[31;1mN\033[0m]:  ' choice
     fi
 
     if [[ $choice =~ ^[Yy] ]]; then
-      [ $verbose ] && echo "Removing \033[36m$dst_path\033[0m"
+      [[ $verbose ]] && echo "Removing \033[36m$dst_path\033[0m"
       rm $dst_path
     else
-      [ $verbose ] && echo "Not removing \033[36m$dst_path\033[0m"
+      [[ $verbose ]] && echo "Not removing \033[36m$dst_path\033[0m"
     fi
   fi
 
-  if [ ! -e $dst_path ] && ln -s $src_path $dst_path; then
-    [ $verbose ] && echo "Link created for \033[36m$dst_path\033[0m"
+  if [[ ! -e $dst_path ]] && ln -s $src_path $dst_path; then
+    [[ $verbose ]] && echo "Link created for \033[36m$dst_path\033[0m"
   else
-    [ $verbose ] && echo "Could not create link for \033[36m$dst_path\033[0m"
+    [[ $verbose ]] && echo "Could not create link for \033[36m$dst_path\033[0m"
   fi
 }
 
@@ -95,11 +95,11 @@ files=()
 # TODO(adam): need to figure out solution for terminal colors
 # .Xresources
 
-[ $setup_bash ] && files+=(.bashrc)
-[ $setup_eslint ] && files+=(.eslintrc.json)
-[ $setup_tmux ] && files+=(.tmux.conf)
+[[ $setup_bash ]] && files+=(.bashrc)
+[[ $setup_eslint ]] && files+=(.eslintrc.json)
+[[ $setup_tmux ]] && files+=(.tmux.conf)
 
-[ $setup_prezto ] && files+=(
+[[ $setup_prezto ]] && files+=(
   .zlogin
   .zlogout
   .zpreztorc
@@ -112,24 +112,24 @@ for filename in ${files[*]}; do
   try_link $this_dir/$filename $HOME/$filename
 done
 
-if [ $setup_neovim ]; then
+if [[ $setup_neovim ]]; then
   nvim_dir=$HOME/.config/nvim
   mkdir -p ${nvim_dir}
   try_link $this_dir/init.vim $nvim_dir/init.vim
 fi
 
-if [ $setup_fzf ]; then
-  if [ ! -x "$(command -v fzf)" ]; then
-    [ $verbose ] && echo "Cloning fzf"
+if [[ $setup_fzf ]]; then
+  if [[ ! -x "$(command -v fzf)" ]]; then
+    [[ $verbose ]] && echo "Cloning fzf"
     git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
-    [ $verbose ] && echo "Running fzf install"
+    [[ $verbose ]] && echo "Running fzf install"
     $HOME/.fzf/install
   else
-    [ $verbose ] && echo "fzf found - not cloning"
+    [[ $verbose ]] && echo "fzf found - not cloning"
   fi
 fi
 
-if [ -x "$(command -v konsole)" ] && [ $konsole_files ]; then
+if [[ -x "$(command -v konsole)" ]] && [[ $konsole_files ]]; then
   konsole_profile_dir=$HOME/.local/share/konsole
   mkdir -p $konsole_profile_dir
   try_link $this_dir/konsole/Mine.profile $konsole_profile_dir/Mine.profile
@@ -137,19 +137,19 @@ if [ -x "$(command -v konsole)" ] && [ $konsole_files ]; then
 fi
 
 # prezto setup
-if [ $setup_prezto ]; then
+if [[ $setup_prezto ]]; then
   if [[ ! -e "${ZDOTDIR:-$HOME}/.zprezto"  ]]; then
-    [ $verbose ] && echo "Cloning Prezto"
+    [[ $verbose ]] && echo "Cloning Prezto"
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
   else
-    [ $verbose ] && echo "Prezto already exists - not cloning"
+    [[ $verbose ]] && echo "Prezto already exists - not cloning"
   fi
 fi
 
-if [ $set_zsh ]; then
+if [[ $set_zsh ]]; then
   # TODO(adam): grep in /etc/shells for zsh
   chsh -s $(which zsh)
 
   # run zsh in current shell
-  [ ! -z $ZSH_NAME ] && exec zsh
+  [[ ! -z $ZSH_NAME ]] && exec zsh
 fi
