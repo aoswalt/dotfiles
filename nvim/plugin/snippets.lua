cf8c34d667d8d756ba22a3f0868655f9ca198548
Adam Oswalt <email@adamoswalt.com>
2021-06-16 11:29:47 -0500

    Add query for largest tables

--- nvim/plugin/snippets.lua
+++ nvim/plugin/snippets.lua
@@ -204,6 +204,16 @@ select view
 from views
 group by view
 order by max(level)]]; -- list dependent views
+    sellargest = [[
+select schemaname as table_schema,
+  relname as table_name,
+  pg_size_pretty(pg_total_relation_size(relid)) as total_size,
+  pg_size_pretty(pg_relation_size(relid)) as data_size,
+  pg_size_pretty(pg_total_relation_size(relid) - pg_relation_size(relid)) as external_size
+from pg_catalog.pg_statio_user_tables
+order by pg_total_relation_size(relid) desc,
+         pg_relation_size(relid) desc
+limit 10]]; -- list 10 largest tables
 
   };
 }
