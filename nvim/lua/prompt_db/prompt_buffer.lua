fee14a8f9f5704f113e1d67301ae2e6276320555
Adam Oswalt <email@adamoswalt.com>
2021-12-09 12:18:12 -0600

    Clean up prompt_buffer a bit
    Still leaves a lingering buffer somehow

--- nvim/lua/prompt_db/prompt_buffer.lua
+++ nvim/lua/prompt_db/prompt_buffer.lua
@@ -13,11 +13,11 @@ function M.prompt_vars(vars, on_submit_)
   on_submit = on_submit_
 
   vim.cmd('new')
-  display_buf()
+  M.display_buf()
 end
 
 function M.complete()
-  vim.cmd('q')
+  vim.cmd('bw')
   on_submit(current_vars)
 end
 
@@ -27,15 +27,16 @@ local function on_complete_prompt(text)
   current_vars[current_prompt_var] = text
   curent_prompt_var = nil
 
-  display_buf()
+  M.display_buf()
 end
 
 function M.prompt_line()
   local line = vim.fn.getline('.')
-  local var = var_from_line(line)
+  local var = M.var_from_line(line)
   current_prompt_var = var
 
   vim.opt.buftype = 'prompt'
+  vim.opt.modifiable = true
 
   local bufnr = vim.fn.bufnr()
   vim.fn.prompt_setcallback(bufnr, on_complete_prompt)
@@ -43,41 +44,39 @@ function M.prompt_line()
   local line = vim.fn.line('$')
   vim.fn.append(line, '=====')
 
+  local current_value = current_vars[var]
+  if current_value == nil or current_value == vim.NIL then
+    current_value = ''
+  end
+
   vim.fn.prompt_setprompt(bufnr, var .. ': ')
 
-  vim.cmd('startinsert')
+  vim.cmd('nnoremap <buffer> <esc> <cmd>lua require("prompt_db.prompt_buffer").display_buf()<cr>')
+
+  vim.cmd('startinsert!')
 end
 
-local function prompt_buf()
-  print_vars()
-
-  vim.opt.buftype = 'prompt'
-
-  local bufnr = vim.fn.bufnr()
-  vim.fn.prompt_setcallback(bufnr, display_buf)
-  vim.fn.prompt_setprompt(bufnr, 'trying things: ')
-
-  vim.cmd('startinsert')
-end
-
-local function display_buf()
-  print_vars()
+function M.display_buf()
+  M.print_vars()
 
   vim.opt.buftype = 'nofile'
+  vim.opt.modifiable = false
 
+  vim.cmd('nnoremap <buffer> <esc> <cmd>bw<cr>')
   vim.cmd('nnoremap <buffer> <cr> <cmd>lua require("prompt_db.prompt_buffer").prompt_line()<cr>')
   vim.cmd('nnoremap <buffer> <m-cr> <cmd>lua require("prompt_db.prompt_buffer").complete()<cr>')
 end
 
-local function print_vars()
-  local rows = make_display(current_vars)
+function M.print_vars()
+  local rows = M.make_display(current_vars)
 
+  vim.opt.modifiable = true
   vim.cmd('%d')
   vim.api.nvim_put(rows, 'l', false, true)
   vim.cmd('.d') --NOTE(adam): remove trailing empty line
 end
 
-local function make_display(vars)
+function M.make_display(vars)
   local rows = {}
   for var, value in pairs(vars) do
     if value == vim.NIL then
@@ -91,7 +90,8 @@ local function make_display(vars)
   return rows
 end
 
-local function var_from_line(line)
+function M.var_from_line(line)
+  -- use prompt_getprompt?
   return string.match(line, '(%w+):')
 end
 
