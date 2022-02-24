239063df153abc2f458752af250a3d97198801a7
Adam Oswalt <email@adamoswalt.com>
2022-02-24 13:10:50 -0600

    Show elixir test results in float window

--- nvim/ftplugin/elixir.lua
+++ nvim/ftplugin/elixir.lua
@@ -1,7 +1,31 @@
 local Job = require('plenary.job')
+local win_float = require('plenary.window.float')
+
+-- stolen from plenary's test_harness
+local function make_output_window()
+  res = win_float.percentage_range_window(0.95, 0.70, { winblend = 3 })
+
+  res.job_id = vim.api.nvim_open_term(res.bufnr, {})
+  vim.api.nvim_buf_set_keymap(res.bufnr, 'n', 'q', ':q<cr>', { noremap = true })
+
+  vim.api.nvim_win_set_option(res.win_id, 'winhl', 'Normal:Normal')
+  vim.api.nvim_win_set_option(res.win_id, "conceallevel", 3)
+  vim.api.nvim_win_set_option(res.win_id, 'concealcursor', 'n')
+
+  if res.border_win_id then
+    vim.api.nvim_win_set_option(res.border_win_id, 'winhl', 'Normal:Normal')
+  end
+
+  if res.bufnr then
+    vim.api.nvim_buf_set_option(res.bufnr, 'filetype', 'ElixirTests')
+  end
+  vim.cmd('mode')
+
+  return res
+end
 
 function elixir_test(type)
-  args = { 'test' }
+  args = { 'test', '--color' }
 
   if type == 'line' then
     file = vim.fn.expand('%')
@@ -18,12 +42,13 @@ function elixir_test(type)
     vim.notify('Running tests for file: ' .. file)
   elseif type == 'all' then
     -- no changes for all
-    vim.notify('Running all tests for file')
+    vim.notify('Running all tests')
   else
     error('Unknown type for tests:', type)
   end
 
-  Job:new({
+  Job
+    :new({
       command = 'mix',
       args = args,
       cwd = vim.fn.getcwd(),
@@ -33,13 +58,16 @@ function elixir_test(type)
             vim.notify('Tests run successfully')
           end)
         else
-          result = table.concat(j:result(), '\n')
           vim.schedule(function()
-            vim.notify(result, vim.diagnostic.severity.ERROR)
+            float = make_output_window()
+            for _, line in ipairs(j:result()) do
+              vim.api.nvim_chan_send(float.job_id, line .. '\r\n')
+            end
           end)
         end
       end,
-    }):start()
+    })
+    :start()
 end
 
 vim.api.nvim_set_keymap('n', '<leader>tt', '<cmd>lua elixir_test("line")<cr>', { noremap = true })
