6d46bf8a514e8c488bc1c64eacf6df825f35e387
Adam Oswalt <email@adamoswalt.com>
2022-02-25 15:48:59 -0600

    Consolidate more zsh settings

--- modules/settings.zsh
+++ modules/settings.zsh
@@ -29,3 +29,15 @@ setopt HIST_BEEP                 # Beep when accessing non-existent history.
 HISTFILE="$HOME/.zhistory"       # The path to the history file.
 HISTSIZE=10000                   # The maximum number of events to save in the internal history.
 SAVEHIST=10000                   # The maximum number of events to save in the history file.
+
+# Directory
+setopt AUTO_CD              # Auto changes to a directory without typing cd.
+setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
+setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
+setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
+setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
+setopt CDABLE_VARS          # Change directory to a path stored in a variable.
+setopt MULTIOS              # Write to multiple descriptors.
+setopt EXTENDED_GLOB        # Use extended globbing syntax.
+unsetopt CLOBBER            # Do not overwrite existing files with > and >>.
+                            # Use >! and >>! to bypass.
