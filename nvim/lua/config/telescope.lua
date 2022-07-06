48706889acab496b4890b350f3dd397d58eb698a
Adam Oswalt <email@adamoswalt.com>
2022-07-06 12:19:46 -0500

    Show untracked files in git prompt

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,6 +1,6 @@
 -- from telescope wiki - use git_files if in git project; fall back to find_files if not
 project_files = function()
-  local ok = pcall(require('telescope.builtin').git_files, {})
+  local ok = pcall(require('telescope.builtin').git_files, { show_untracked = true })
   if not ok then
     require('telescope.builtin').find_files({ hidden = true })
   end
@@ -12,18 +12,6 @@ require('telescope').setup({
   defaults = {
     file_sorter = require('telescope.sorters').get_fzy_sorter,
     generic_sorter = require('telescope.sorters').get_fzy_sorter,
-    -- prompt_prefix = '',  -- fixes using rsi in prompt but causes other issues
-    vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-      '--hidden',
-      [[--glob=!.git']],
-    },
     mappings = {
       i = {
         -- ["<esc>"] = actions.close,
