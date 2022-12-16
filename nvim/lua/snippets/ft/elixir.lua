dfb904f58b832d08c5349cf812beefba2b32dc19
Adam Oswalt <email@adamoswalt.com>
2022-12-16 13:15:20 -0600

    Add elixir inspect variants and key-value snippets

--- nvim/lua/snippets/ft/elixir.lua
+++ nvim/lua/snippets/ft/elixir.lua
@@ -1,5 +1,7 @@
 -- luacheck: new globals s sn t f i c d r l rep p m n dl fmt fmta conds types events parse ai
 
+local function filepath() return vim.fn.expand('%') end
+
 return {
   s(
     'def',
@@ -54,8 +56,12 @@ return {
     c(1, {
       fmt('IO.inspect({}, label: "{}")', { i(1), dl(2, l._1, 1) }),
       fmt('|> IO.inspect(label: "{}")', { i(1) }),
+      fmt('IO.inspect(<>, label: "<>:#{__ENV__.line}")', { i(1), f(filepath, {}) }, { delimiters = '<>' }),
+      fmt('|> IO.inspect(label: "[]:#{__ENV__.line}")', { f(filepath, {}) }, { delimiters = '[]' }),
     })
   ),
+
+  s('k', fmt('{}: {}', { i(1), dl(2, l._1, 1) })),
 
   s(
     'doc',
