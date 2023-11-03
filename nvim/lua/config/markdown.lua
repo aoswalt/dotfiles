5eac65fa5394981fbd8807d8b3dc716e5e60bff9
Adam Oswalt <email@adamoswalt.com>
2023-11-03 08:36:42 -0500

    Set borderless table for :TableFormat

--- nvim/lua/config/markdown.lua
+++ nvim/lua/config/markdown.lua
@@ -2,3 +2,4 @@ vim.g.vim_markdown_toc_autofit = 1
 vim.g.vim_markdown_frontmatter = 1
 vim.g.vim_markdown_toml_frontmatter = 1
 vim.g.vim_markdown_new_list_item_indent = 2
+vim.g.vim_markdown_borderless_table = 1
